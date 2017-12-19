1b3a5864405850d3b6ca90dcf68635a487b67c26
Qiao <haoqiao@kpmg.ca>
2017-12-19 15:11:49 -0500

    finished

--- index.js
+++
@@ -1,8 +0,0 @@
-(function ($, jQuery) {
-
-    $('#submit').click(function () {
-        $('#board').append($('<div id="item"></div>'));
-        $('#item').height($('#item').width() / 6);
-    });
-
-}($, jQuery));
